0c33006c9b96050ed7fce24476b148b7fa7e97a9
HumeyraTEZCAN <44144939+HumeyraTEZCAN@users.noreply.github.com>
2022-05-27 17:19:32 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  randomStart: true,
+			  auto: true,
+			  speed:3,
+			  pager: true,
+			  pagerType:"short",
+			  pagerShortSeparator: "/"
+			});
+    	});
